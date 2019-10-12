38c60a5b54d97179dc9f15e6f19479f25a3df725
AlreadyBored <shilaums@gmail.com>
2019-10-12 17:46:12 +0300

    init: start multiply task & all test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  // HOW IT SHOULD LOOK LIKE SECTION
+  /* const firstExact = BigInt(first);
+  const secondExact = BigInt(second);
+  return `${firstExact * secondExact}`; */
+  
+  // REVERSE CHUNKY MULTIPLYING SECTION
+  const firstChunkyReversed = first.split('').reverse();
+  const secondChunkyReversed = second.split('').reverse();
+
+  const buffer = [];
+
+  for(let i = 0; i < firstChunkyReversed.length; i++){
+
+    for(let j = 0; j < secondChunkyReversed.length; j++) {
+
+      const containerIndex = [i + j];
+
+      const currProduct = firstChunkyReversed[i] * secondChunkyReversed[j];
+
+      buffer[containerIndex] ? buffer[containerIndex] += currProduct 
+      : buffer[containerIndex] = currProduct;
+    }
+  }
+
+  for(let i = 0; i < buffer.length; i++) {
+
+    const nextInd = i + 1;
+
+    const numericalPart = buffer[i] % 10;
+    const decimalShift = Math.floor(buffer[i] / 10);
+
+    buffer[i] = numericalPart;
+    
+    buffer[nextInd] ? buffer[nextInd] += decimalShift
+    : decimalShift ? 
+    buffer[nextInd] = decimalShift : null;
+  }
+
+  const res = buffer.reverse().join('');
+
+  return res;
+
 }
